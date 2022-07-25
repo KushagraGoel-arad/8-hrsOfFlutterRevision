778005dac06ccd21a6cb61b67f0c681c5707a157
Kushagra Goel <kushagra.goel@gmail.com>
2022-07-25 14:50:02 +0530

    day 4 complete

--- codepurpractice/lib/main.dart
+++ codepurpractice/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:codepurpractice/pages/home_page.dart';
 import 'package:codepurpractice/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: ThemeData(
+          primarySwatch: Colors.green,
+          
+          fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(brightness: Brightness.light),
       initialRoute: "/home",
       routes: {
